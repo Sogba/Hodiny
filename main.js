d765c9a49cbd151bbf3c095b90612b45a830e872
Sogba <gametik00@gmail.com>
2025-10-28 13:53:10 +0100

    Finished

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
 var timeShift = 0;
 
+const alert = {
+    warning: 1,
+    error: 2,
+    information: 3
+}
+
+
+
+function myAlert(message, typeOfAlert) {
+    const panel = document.querySelector('.myAlertPanel');
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('myAlert');
+
+    switch (typeOfAlert) {
+        case alert.warning:
+            alertDiv.style.backgroundColor = '#f6a62dff';
+            break;
+        case alert.error:
+            alertDiv.style.backgroundColor = '#f16156ff';
+            break;
+        case alert.information:
+            alertDiv.style.backgroundColor = '#3996e2ff';
+            break;
+        default:
+            alertDiv.style.backgroundColor = '#9E9E9E';
+            break;
+    }
+
+    const p = document.createElement('p');
+    p.textContent = message;
+    alertDiv.appendChild(p);
+
+    alertDiv.style.opacity = 0;
+    alertDiv.style.transition = 'opacity 0.3s ease';
+
+    panel.appendChild(alertDiv);
+
+    
+    requestAnimationFrame(() => {
+        alertDiv.style.opacity = 1;
+    });
+
+    setTimeout(() => {
+        alertDiv.style.opacity = 0;
+        setTimeout(() => alertDiv.remove(), 300); // wait for fade-out
+    }, 5000);
+    
+}
+
 
 function changeValue(elementId, newValue){
     if(newValue < 10)
@@ -28,6 +77,26 @@ function setDate(){
     var currentDate = "" + daysOfWeek[date.getDay()] + " " + date.getDate() + ". " + monthsOfYear[date.getMonth()] + " " + date.getFullYear();
 
     changeValue("date", currentDate)
+}
+
+function updateTimeShift(){
+    let input = Number.parseInt(document.getElementById("timeShiftInput").value);
+    
+
+    if(!Number.isInteger(input)){
+        document.getElementById("settings").style.visibility="hidden";
+        myAlert("Value in time shift isnt number", alert.error);
+        return;
+    }
+
+    timeShift = input;
+    myAlert("Time shift has been saved", alert.information)
+    document.getElementById("settings").style.visibility="hidden";
+}
+
+function showSettings(){
+    document.getElementById("settings").style.visibility="visible";
+    console.log("Something happend")
 }
 
 setDate();
